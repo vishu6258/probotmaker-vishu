bbd5d8e9e5d910cc5aa9105f1be93d6ff614560f
vishu6258 <84761225+vishu6258@users.noreply.github.com>
2021-05-25 11:59:57 +0530

    Update withatron.js

--- commands/Withdrawal/withatron.js
+++ commands/Withdrawal/withatron.js
@@ -1,1 +1,32 @@
+/*CMD
+  command: withatron
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Withdrawal
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
 
+let stat = Bot.getProperty(user.telegramid)
+let wdinfo = Bot.getProperty("wdInfo")
+if (!stat) {
+  
+  let balance = Libs.ResourcesLib.userRes("balance")
+  let withdrawn = Libs.ResourcesLib.userRes("withdrawn")
+  var wallet = User.getProperty("TRXwallet")
+  if (wallet == undefined) {
+    Bot.sendMessage("_❌ wallet Not set_")
+  } else {
+    if (balance.value() < 100000) {
+      Bot.sendMessage("_❌ You have to own at least 100K ATRON!_")
+    } else {
+      Bot.sendMessage("*📤 Enter Amount of ATRON*")
+      Bot.runCommand("With1")
+    }
+  }
+
+}else{Bot.sendMessage("*You're Ban you can withdraw only if Balance up then 1000k trxtweet because i lost my all energy to make transactions*")
+return
+}
